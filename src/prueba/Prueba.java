168450996f5de20211d3ce5361ae1410dc7ff912
Teresa Garcia <garcia.beltran.teresa.de.los.angeles@iescamas.es>
2025-01-05 21:48:29 +0100

    inicio

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author teres
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
